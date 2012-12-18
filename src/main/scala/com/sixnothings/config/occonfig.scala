ec1a7c088bccbf00e301fdbfc67278f4e5fec723
Daniel Trinh <me@daniel.trinh>
2012-12-18 00:33:13 -0800

    cleaned up ambiguity between typesafe config and application settings a bit, and added an agent for updating the twitter config daily.

--- src/main/scala/com/sixnothings/config/occonfig.scala
+++ src/main/scala/com/sixnothings/config/occonfig.scala
@@ -1,56 +1,59 @@
 package com.sixnothings.config
-
+import scala.io.Source
 import com.typesafe.config._
 import com.ning.http.client.oauth._
+import com.codahale.jerkson.Json._
+import com.sixnothings.twitter.json._
+import akka.agent.Agent
+import com.sixnothings.maestro.MySystem
 
-trait ProjectConfig {
+
+trait ProjectSettings {
   val base = ConfigFactory.load()
 }
 
-case object TwitterConfig extends ProjectConfig {
+case object TwitterSettings extends ProjectSettings {
+  val source = Source.fromURL(getClass.getResource("/defaultTwitterConfiguration.json"))
+  val twitterConfiguration = Agent(parse[TwitterConfiguration](source))(MySystem())
 
-  val twitterConfig = base.getConfig("twitter")
-  val handlesConfig = twitterConfig.getConfig("handles")
-  val twitterUrlsConfig = twitterConfig.getConfig("urls")
+  // TODO: figure out less verbose way of loading values from conf..
+  // probably use a .json file with jerkson instead.
+  val twitterConf = base.getConfig("twitter")
+  val handlesConf = twitterConf.getConfig("handles")
+  val twitterUrlsConf = twitterConf.getConfig("urls")
 
-  val charLimit = twitterConfig.getInt("charLimit")
+  val charLimit = twitterConf.getInt("charLimit")
 
   val ocremixHandle = TwitterHandle(
     "ocremix",
-    handlesConfig.getConfig("ocremix").getString("id")
+    handlesConf.getConfig("ocremix").getString("id")
   )
 
   val panicHandle = TwitterHandle(
     "panic",
-    handlesConfig.getConfig("panic").getString("id")
+    handlesConf.getConfig("panic").getString("id")
   )
 
-  val consumerKey       = new ConsumerKey(
-    twitterConfig.getString("consumerKey"),
-    twitterConfig.getString("consumerSecret")
+  val consumerKey = new ConsumerKey(
+    twitterConf.getString("consumerKey"),
+    twitterConf.getString("consumerSecret")
   )
 
   val accessKey = new RequestToken(
-    twitterConfig.getString("accessKey"),
-    twitterConfig.getString("accessSecret")
+    twitterConf.getString("accessKey"),
+    twitterConf.getString("accessSecret")
   )
-
 
   val urlKeys = List("requestToken", "accessToken", "authorize", "api")
 
   val twitterUrls = urlKeys.foldLeft(Map[String, String]()) {(
-    (urls, key) => urls + (key -> twitterUrlsConfig.getString(key)))
+    (urls, key) => urls + (key -> twitterUrlsConf.getString(key)))
   }
 
 }
 
-case object OCConfig {
-  // TODO: figure out less verbose way of loading values from conf..
-  // probably use a .json file with jerkson instead.
-
-  val base = ConfigFactory.load()
+case object OCRemixSettings extends ProjectSettings {
   val rssUrl = base.getConfig("ocremix").getConfig("urls").getString("ocremixRss")
-
 }
 
 case class TwitterHandle(screenName: String, id: String)
